0a8daba3c8251de9a53a2a149405e2743b6408bc
EvilDogma <512gallagher@gmail.com>
2024-04-02 16:15:47 -0400

    added more functions to sandboxJS

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -6,8 +6,6 @@ var monthVal = today.format('MM')
 var dayVal = today.format('DD')
 var countryVal = "US"
 
-
-
 var paramsForHoliday = new URLSearchParams({
     api_key: "4657a48c034a4f669efcb46c03aa821f",
     country: countryVal,
@@ -22,88 +20,68 @@ function getHolidays(dateObj) {
     paramsForHoliday.set("year", `${dateObj.year}`);
     paramsForHoliday.set("month", `${dateObj.month}`);
     paramsForHoliday.set("day", `${dateObj.day}`);
-    fetch(`${urlForHoliday}?${paramsForHoliday}`)
+    return fetch(`${urlForHoliday}?${paramsForHoliday}`)
         .then(response => response.json())
-        .then(response => console.log(response))
+        // .then(response => console.log(response))
         .catch(err => console.error(err));
 
 }
-// fetch(`${urlForHoliday}?${paramsForHoliday}`)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-
-// fetch('https://holidays.abstractapi.com/v1/?api_key=4657a48c034a4f669efcb46c03aa821f&country=US&year=2024&month=04&day=04')
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-
-
 
 function getUserInput(event) {
     event.preventDefault()
-    const Date = $('#date').val().split('-')
-    const year = Date[0]
-    const Month = Date[1]
-    const Day = Date[2]
-    const DateOBJ = {
+    const inputDate = $('#date').val().split('-')
+    const year = inputDate[0]
+    const month = inputDate[1]
+    const day = inputDate[2]
+    const dateObj = {
         year: year,
-        month: Month,
-        day: Day
+        month: month,
+        day: day
     }
-    console.log(DateOBJ)
-    getHolidays(DateOBJ)
-    return DateOBJ;
+    console.log(dateObj)
+    // getHolidays(dateObj)
+    //     .then(renderHolidays)
+    return dateObj;
 }
-$(document).ready(function () {
-    fetch(`${urlForHoliday}?${paramsForHoliday}`)
+function renderNewHolidays(event) {
+    const dateObj = getUserInput(event)
+    getHolidays(dateObj)
+        .then(renderHolidays)
+}
+function renderHolidays(holidays) {
+    holidays.forEach(holiday => $('#holidays').append(`<div class="holiday">${holiday.name}</div>`))
+}
+
+function getTodaysHolidays() {
+    return fetch(`${urlForHoliday}?${paramsForHoliday}`)
         .then(response => response.json())
         .then(response => response.forEach(holiday => $('#holidays').append(`<div class="holiday">${holiday.name}</div>`)))
         .catch(err => console.error(err));
-
-    // $('.holidays')
+}
+$(document).ready(function () {
+    getTodaysHolidays()
+    // fetch(`${urlForHoliday}?${paramsForHoliday}`)
+    //     .then(response => response.json())
+    //     .then(response => response.forEach(holiday => $('#holidays').append(`<div class="holiday">${holiday.name}</div>`)))
+    //     .catch(err => console.error(err));
 });
 
-function displayModel() {
-    console.log("")
-}
-
-$('form').on('submit', getUserInput)
+$('form').on('submit', renderNewHolidays)
 $('#holidays').on('click', '.holiday', function (event) {
     console.log('Clicked on ' + this.innerHTML)
-    getWikiArticalArray(this.innerHTML)
+    getWikiArticalArray(this.innerHTML, countryVal)
         .then(getWikiExerpt)
+        .then(exerpt => console.log(exerpt))
+    // .then(openModalWithExerpt)
 })
 
-function getWikiExerpt(pageId) {
-    const url = "https://en.wikipedia.org/w/api.php";
-    var params2 = new URLSearchParams({
-        action: "query",
-        format: "json",
-        prop: "extracts",
-        titles: pageId,
-        formatversion: "2",
-        exsentences: "10",
-        exlimit: "1",
-        explaintext: 1,
-        origin: "*"
-    });
-    console.log(`${params2}`)
-    fetch(`${url}?${params2}`)
-        .then(response => response.json())
-        .then(response => console.log(response.query.pages[0].extract))
-        .catch(err => console.error(err));
-}
-
-
-function getWikiArticalArray(holiday) {
+// Takes holiday - Returns title of first relevan wikipedia article
+function getWikiArticalArray(holiday, country) {
     const url = "https://en.wikipedia.org/w/api.php";
     const params = new URLSearchParams({
         action: "query",
         list: "search",
-        srsearch: holiday,
+        srsearch: holiday + ' ' + country,
         format: "json",
         origin: "*"
     });
@@ -111,40 +89,25 @@ function getWikiArticalArray(holiday) {
         .then(response => response.json())
         .then(response => response.query.search[0].title)
         .catch(err => console.error(err));
-
-
 }
-// var url = "https://en.wikipedia.org/w/api.php";
+// Takes title or article - returns exerpt text
 
-// var params = new URLSearchParams({
-//     action: "query",
-//     list: "search",
-//     srsearch: "United Nations' Mine Awareness Day",
-//     format: "json",
-//     origin: "*"
-// });
-// console.log(`${url}?${params}`)
-
-// fetch(`${url}?${params}`)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-// var params2 = new URLSearchParams({
-//     action: "query",
-//     format: "json",
-//     prop: "extracts",
-//     titles: "",
-//     formatversion: "2",
-//     exsentences: "10",
-//     exlimit: "1",
-//     explaintext: 1,
-//     origin: "*"
-// });
-
-// params2.set("titles", "United Nations Mine Action Service");
-
-// fetch(`${url}?${params2}`)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
+function getWikiExerpt(title) {
+    const url = "https://en.wikipedia.org/w/api.php";
+    var params2 = new URLSearchParams({
+        action: "query",
+        format: "json",
+        prop: "extracts",
+        titles: title,
+        formatversion: "2",
+        exsentences: "10",
+        exlimit: "1",
+        explaintext: 1,
+        origin: "*"
+    });
+    console.log(`${params2}`)
+    return fetch(`${url}?${params2}`)
+        .then(response => response.json())
+        .then(response => response.query.pages[0].extract)
+        .catch(err => console.error(err));
+}
